77686f8e49b6125ec87cc202657be4a4e3b44ff1
smeghead <smeghead>
2007-10-29 12:07:05 +0000

    add user.css.

---
+++ css.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <cgic.h>
+#include "css.h"
+#include "util.h"
+
+#define MAX_WIDTH 10000
+
+void css_content_out(char* filename)
+{
+    FILE* in;
+    char line[MAX_WIDTH];
+
+    if ((in  = fopen(filename, "r")) == NULL)
+        die("file open error.");
+    while (fgets(line, sizeof(line), in)) {
+        h(line);
+    }
+    fclose(in);
+}
+void css_save(char* filename, char* content)
+{
+    FILE* out;
+
+    /* TODO ロック処理 */
+    if ((out  = fopen(filename, "w")) == NULL)
+        die("file open error.");
+    fputs(content, out);
+    fclose(out);
+}
+
---
+++ css.h
@@ -0,0 +1,7 @@
+#ifndef CSS_H
+#define CSS_H
+
+void css_content_out(char* filename);
+void css_save(char* filename, char* content);
+
+#endif
