40de7e4e10739685b8602a064b37cd83d2d9bff7
smeghead <smeghead>
2010-08-21 10:57:00 +0000

    add svn connection system.

---
+++ script/post-commit-svn.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+use LWP::UserAgent;
+use HTTP::Request::Common qw(POST);
+
+my $starbug1_info = {
+    submit_url => 'http://localhost/starbug1/index.cgi/Sand%20Box/register_submit',
+    comment_field_id => 8
+};
+
+my $commit_info = {
+    repos => "$1",
+    rev => $2,
+    log => `svnlook log -r $rev "$repos"`,
+    author => `svnlook author -r $rev "$repos"`
+};
+
+sub change_status {
+    my ($ticket_id, $status, $starbug1_info, $commit_info) = @_;
+    my %params = (
+        reply => 1,
+        ticket_id => $ticket_id,
+        field2 => $commit_info->{author},
+        field3 => $status,
+    );
+    $params{'field' . $starbug1_info->{comment_field_id}} = $commit_info->{log};
+
+    use Data::Dumper;
+    print Dumper(\%params);
+    # POST のパラメータを渡す際に [] を忘れないようにする
+    my $request = POST($starbug1_info->{submit_url}, [%params]);
+
+    my $ua = LWP::UserAgent->new;
+    my $response = $ua->request($request);
+
+    print 'post-commit-svn: ', $response->status_line, "\n";
+}
+
+if ($commit_info->{log} =~ m{\#(\d+) (\S+)}m) {
+    print "$1 $2\n";
+    change_status($1, $2, $starbug1_info, $commit_info);
+}
